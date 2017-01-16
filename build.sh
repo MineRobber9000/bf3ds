47e65083de739a80c97feae11b056e845cfa4096
MineRobber9000 <MineRobber9000@users.noreply.github.com>
2017-01-16 08:36:01 +0000

    Added web page

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm bf3ds.*
-make
-mv bf3ds.3dsx dist
-mv bf3ds.smdh dist
--- src/bf.c
+++
@@ -1,4 +0,0 @@
-// Brainfuck tape
-unsigned char tape[30000] = {0};
-// Brainfuck pointer
-unsigned char* ptr = tape;
