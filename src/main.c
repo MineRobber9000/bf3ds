8ec567dfae7d58305c018008c3d1009c7f308a1c
MineRobber9000 <MineRobber9000@users.noreply.github.com>
2017-01-16 10:01:09 +0000

    Demo-ready; buttons used for placeholder input

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <3ds.h>
 #include <stdio.h>
+#include <string.h>
+
+char program[4096] = {(char)0};
 
 int main(int argc, char **argv) {
 	gfxInitDefault();
@@ -11,6 +14,29 @@ int main(int argc, char **argv) {
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 			break;
+		if (kDown & KEY_B) {
+			strcat(program,">");
+		//	printf("\x1b[2;0%s",program);
+		}
+		if (kDown & KEY_A) {
+			strcat(program,"<");
+		}
+		if (kDown & KEY_Y) {
+			strcat(program,"+");
+		}
+		if (kDown & KEY_X) {
+			strcat(program,"-");
+		}
+		if (kDown & KEY_UP) {
+			strcat(program,".");
+		}
+		if (kDown & KEY_LEFT) {
+			strcat(program,"[");
+		}
+		if (kDown & KEY_RIGHT) {
+			strcat(program,"]");
+		}
+		printf("\x1b[2;0H%s",program);
 		gfxFlushBuffers();
 		gfxSwapBuffers();
 	}
